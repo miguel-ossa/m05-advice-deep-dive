611a65439dfc6a09edfd0ccbb1bc63d5dd5b2513
Miguel de la Ossa <miguel.ossa.abellan@gmail.com>
2019-06-01 15:53:04 +0200

    Initial commit

---
+++ src/main/java/advicedeepdive/BeforeAdvice.java
@@ -0,0 +1,35 @@
+package advicedeepdive;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class BeforeAdvice {
+
+  Logger logger = LoggerFactory.getLogger(BeforeAdvice.class);
+
+  private boolean beforeCalled = false;
+
+  public void reset() {
+    beforeCalled = false;
+  }
+
+  public boolean isBeforeCalled() {
+    return beforeCalled;
+  }
+
+  @Before("execution(void doSomething())")
+  public void entering(JoinPoint joinPoint) {
+//	  System.out.println("I am here");
+    beforeCalled = true;
+    logger.trace("entering "
+        + joinPoint.getStaticPart().getSignature().toString());
+//	  System.out.println("I am nowhere");
+  }
+
+}
